73a17829cdf965a4426d6bcb5232e2bd1e41757b
Vipinraj <atkvipin@gmail.com>
2026-06-14 12:09:01 +0530

    Add google callback debug

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,8 @@ import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import userSchema from '../model/userModel.js';
 import { getReferralCode } from '../middleware/captureReferral.js';
 
+console.log('GOOGLE_CALLBACK_URL =', process.env.GOOGLE_CALLBACK_URL);
+
 passport.use(
   new GoogleStrategy(
     {
