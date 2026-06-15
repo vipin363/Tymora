ae38f948b1744a45ac93a06f1ea935d039bb38f0
Vipinraj <atkvipin@gmail.com>
2026-06-15 14:36:24 +0530

    debugging the session

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,21 +1,26 @@
-import User from "../model/userModel.js";
+import User from '../model/userModel.js';
 
-export const isLogin=(req,res,next)=>{
-    if(req.session.user){
-        res.redirect('/user/')
-    }else{
-        next()
-    }
-}
+export const isLogin = (req, res, next) => {
+  if (req.session.user) {
+    res.redirect('/user/');
+  } else {
+    next();
+  }
+};
 
 export const isAuth = async (req, res, next) => {
   try {
+    console.log('SESSION ID:', req.sessionID);
+    console.log('SESSION USER:', req.session.user);
 
     if (!req.session.user) {
-       if (req.xhr ||req.headers['content-type']?.includes('application/json') ||
-    req.path.startsWith('/api/')) {
-    return res.json({ success: false, redirect: '/user/login' });
-  }
+      if (
+        req.xhr ||
+        req.headers['content-type']?.includes('application/json') ||
+        req.path.startsWith('/api/')
+      ) {
+        return res.json({ success: false, redirect: '/user/login' });
+      }
       return res.redirect('/user/login');
     }
 
@@ -23,48 +28,49 @@ export const isAuth = async (req, res, next) => {
 
     if (!user) {
       req.session.user = null;
-      return res.redirect('/user/?message=Your account has been deleted by admin');
+      return res.redirect(
+        '/user/?message=Your account has been deleted by admin'
+      );
     }
 
     if (user.isBlocked) {
       req.session.user = null;
-      return res.redirect('/user/?message=Your account has been blocked by admin');
+      return res.redirect(
+        '/user/?message=Your account has been blocked by admin'
+      );
     }
-   
 
     next();
-
   } catch (err) {
     console.log(err);
     res.redirect('/user/login');
   }
 };
 
-export const hasOtpSession = (req,res,next)=>{
-   if(req.session.userData){
-      next();
-   }else{
-      res.redirect('/user/register');
-   }
-}
+export const hasOtpSession = (req, res, next) => {
+  if (req.session.userData) {
+    next();
+  } else {
+    res.redirect('/user/register');
+  }
+};
 
-export const onlyPublic = (req,res,next)=>{
-   next();
-}
+export const onlyPublic = (req, res, next) => {
+  next();
+};
 
-export const hasForgotSession = (req,res,next)=>{
-   if(req.session.resetEmail){
-      return next();
-   }
+export const hasForgotSession = (req, res, next) => {
+  if (req.session.resetEmail) {
+    return next();
+  }
 
-   return res.redirect('/user/forgotPassword');
-}
+  return res.redirect('/user/forgotPassword');
+};
 
-export const hasResetVerified = (req,res,next)=>{
-   if(req.session.resetVerified){
-      next();
-   }else{
-      return res.redirect('/user/login');
-   }
-}
-
+export const hasResetVerified = (req, res, next) => {
+  if (req.session.resetVerified) {
+    next();
+  } else {
+    return res.redirect('/user/login');
+  }
+};
